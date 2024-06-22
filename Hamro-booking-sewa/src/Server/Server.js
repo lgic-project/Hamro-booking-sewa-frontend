b6ed697abc602439062951a1ae74a57e6a4167d6
Ujjwal Adhikari <pokhreli.thito10@gmail.com>
2024-06-22 13:34:36 +0545

    added category selection in the register page

--- Hamro-booking-sewa/src/Server/Server.js
+++ Hamro-booking-sewa/src/Server/Server.js
@@ -1,5 +1,5 @@
 const Server = {
-    primaryUrl : "http://192.168.1.16:8000",
+    primaryUrl : "http://192.168.1.2:8000",
     secondaryUrl : "http://127.0.0.1:8000",
     emulatorUrl : "http://10.0.2.2:8000",
     consultancy : "http://192.168.1.16",
--- Hamro-booking-sewa/src/screens/RegisterScreen/RegisterScreen.js
+++ Hamro-booking-sewa/src/screens/RegisterScreen/RegisterScreen.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, TouchableOpacity, Alert } from 'react-native';
-import { Text } from 'react-native-paper';
+import { View, StyleSheet, TouchableOpacity, Alert, ScrollView } from 'react-native';
+import { Text, TextInput as PaperTextInput } from 'react-native-paper';
 import Background from '../../components/Background';
 import Logo from '../../components/Logo';
 import Header from '../../components/Header';
 import Button from '../../components/Button';
-import TextInput from '../../components/TextInput';
 import BackButton from '../../components/BackButton';
 import { theme } from '../../core/theme';
 import { emailValidator } from '../../helpers/emailValidator';
@@ -14,6 +13,7 @@ import { nameValidator } from '../../helpers/nameValidator';
 import { categoryNameValidator } from '../../helpers/categoryNameValidator';
 import { phoneNumberValidator } from '../../helpers/phoneNumberValidator';
 import Server from '../../Server/Server';
+import ModalSelector from 'react-native-modal-selector';
 
 export default function RegisterScreen({ navigation }) {
   const [name, setNameName] = useState({ value: '', error: '' });
@@ -22,22 +22,20 @@ export default function RegisterScreen({ navigation }) {
   const [password, setPassword] = useState({ value: '', error: '' });
   const [phone_number, setPhoneNumber] = useState({ value: '', error: '' });
   const [csrfToken, setCsrfToken] = useState('');
+  const [textInputValue, setTextInputValue] = useState('');
+
+  const data = [
+    { key: 0, label: 'Hotel', value: 'hotel' },
+    { key: 1, label: 'User', value: 'user' },
+  ];
 
   useEffect(() => {
     fetch(Server.primaryUrl+'/csrf-token', {
       method: 'GET',
       credentials: 'include', // Include cookies if necessary
     })
-      .then(response => {
-        console.log('Response status:', response.status);
-        console.log('Response headers:', response.headers);
-
-        return response.text(); // Read the response as text first
-      })
+      .then(response => response.text())
       .then(text => {
-        console.log('Response text:', text);
-
-        // Try to parse as JSON
         let data;
         try {
           data = JSON.parse(text);
@@ -55,13 +53,13 @@ export default function RegisterScreen({ navigation }) {
 
   const onSignUpPressed = () => {
     const nameError = nameValidator(name.value);
-    const lastNameError = categoryNameValidator(category.value);
+    const categoryError = categoryNameValidator(category.value);
     const emailError = emailValidator(email.value);
     const passwordError = passwordValidator(password.value);
     const phoneNumberError = phoneNumberValidator(phone_number.value);
-    if (emailError || passwordError || nameError || lastNameError || phoneNumberError) {
+    if (emailError || passwordError || nameError || categoryError || phoneNumberError) {
       setNameName({ ...name, error: nameError });
-      setCategoryName({ ...category, error: lastNameError });
+      setCategoryName({ ...category, error: categoryError });
       setEmail({ ...email, error: emailError });
       setPassword({ ...password, error: passwordError });
       setPhoneNumber({ ...phone_number, error: phoneNumberError });
@@ -103,80 +101,141 @@ export default function RegisterScreen({ navigation }) {
   return (
     <Background>
       <BackButton goBack={navigation.goBack} />
-      <Logo />
-      <Header>Fill up your details</Header>
-      <TextInput
-        label="First Name"
-        returnKeyType="next"
-        value={name.value}
-        onChangeText={(text) => setNameName({ value: text, error: '' })}
-        error={!!name.error}
-        errorText={name.error}
-      />
-      <TextInput
-        label="Category"
-        returnKeyType="next"
-        value={category.value}
-        onChangeText={(text) => setCategoryName({ value: text, error: '' })}
-        error={!!category.error}
-        errorText={category.error}
-      />
-      <TextInput
-        label="Email"
-        returnKeyType="next"
-        value={email.value}
-        onChangeText={(text) => setEmail({ value: text, error: '' })}
-        error={!!email.error}
-        errorText={email.error}
-        autoCapitalize="none"
-        autoCompleteType="email"
-        textContentType="emailAddress"
-        keyboardType="email-address"
-      />
-      <TextInput
-        label="Password"
-        returnKeyType="done"
-        value={password.value}
-        onChangeText={(text) => setPassword({ value: text, error: '' })}
-        error={!!password.error}
-        errorText={password.error}
-        secureTextEntry
-      />
-      <TextInput
-        label="Phone Number"
-        returnKeyType="done"
-        value={phone_number.value}
-        onChangeText={(text) => setPhoneNumber({ value: text, error: '' })}
-        error={!!phone_number.error}
-        errorText={phone_number.error}
-        keyboardType="numeric"
-      />
-      <Button
-        mode="outlined"
-        onPress={onSignUpPressed}
-        style={{ marginTop: 24, justifyContent: "center" }}
-      >
-        Register
-      </Button>
-      <View style={styles.row}>
-        <Text>I already have an account !</Text>
-      </View>
-      <View style={styles.row}>
-        <TouchableOpacity onPress={() => navigation.replace('LoginScreen')}>
-          <Text style={styles.link}>Log in</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Logo />
+        <Header>Fill up your details</Header>
+        <View style={styles.inputContainer}>
+          <PaperTextInput
+            label="Full Name"
+            mode="outlined"
+            returnKeyType="next"
+            value={name.value}
+            onChangeText={(text) => setNameName({ value: text, error: '' })}
+            error={!!name.error}
+            style={styles.input}
+          />
+          {name.error ? <Text style={styles.error}>{name.error}</Text> : null}
+        </View>
+        <View style={styles.inputContainer}>
+          
+          <ModalSelector
+            data={data}
+            initValue="Select a category"
+            onChange={(option) => {
+              setCategoryName({ value: option.value, error: '' });
+              setTextInputValue(option.label);
+            }}
+            accessible={true}
+            keyExtractor={(item) => item.key.toString()}
+            labelExtractor={(item) => item.label}
+          >
+            <TouchableOpacity>
+              <PaperTextInput
+                mode="outlined"
+                editable={false}
+                placeholder="Select a category"
+                value={textInputValue}
+                style={styles.input}
+              />
+            </TouchableOpacity>
+          </ModalSelector>
+          {category.error ? <Text style={styles.error}>{category.error}</Text> : null}
+        </View>
+        <View style={styles.inputContainer}>
+          <PaperTextInput
+            label="Email"
+            mode="outlined"
+            returnKeyType="next"
+            value={email.value}
+            onChangeText={(text) => setEmail({ value: text, error: '' })}
+            error={!!email.error}
+            autoCapitalize="none"
+            autoCompleteType="email"
+            textContentType="emailAddress"
+            keyboardType="email-address"
+            style={styles.input}
+          />
+          {email.error ? <Text style={styles.error}>{email.error}</Text> : null}
+        </View>
+        <View style={styles.inputContainer}>
+          <PaperTextInput
+            label="Password"
+            mode="outlined"
+            returnKeyType="done"
+            value={password.value}
+            onChangeText={(text) => setPassword({ value: text, error: '' })}
+            error={!!password.error}
+            secureTextEntry
+            style={styles.input}
+          />
+          {password.error ? <Text style={styles.error}>{password.error}</Text> : null}
+        </View>
+        <View style={styles.inputContainer}>
+          <PaperTextInput
+            label="Phone Number"
+            mode="outlined"
+            returnKeyType="done"
+            value={phone_number.value}
+            onChangeText={(text) => setPhoneNumber({ value: text, error: '' })}
+            error={!!phone_number.error}
+            keyboardType="numeric"
+            style={styles.input}
+          />
+          {phone_number.error ? <Text style={styles.error}>{phone_number.error}</Text> : null}
+        </View>
+        <Button
+          mode="outlined"
+          onPress={onSignUpPressed}
+          style={styles.button}
+        >
+          Register
+        </Button>
+        <View style={styles.row}>
+          <Text>I already have an account!</Text>
+          <TouchableOpacity onPress={() => navigation.replace('LoginScreen')}>
+            <Text style={styles.link}> Log in</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </Background>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputContainer: {
+    width: '100%',
+    marginVertical: 10,
+  },
+  input: {
+    backgroundColor: 'white',
+  },
+  label: {
+    marginBottom: 5,
+    fontSize: 16,
+  },
+  error: {
+    fontSize: 14,
+    color: 'red',
+    marginTop: 5,
+  },
+  button: {
+    marginTop: 24,
+    padding: 8,
+    width: '100%',
+  },
   row: {
     flexDirection: 'row',
-    marginTop: 4,
+    marginTop: 24,
+    justifyContent: 'center',
   },
   link: {
-    fontWeight: '800',
+    fontWeight: 'bold',
     color: theme.colors.primary,
   },
 });
